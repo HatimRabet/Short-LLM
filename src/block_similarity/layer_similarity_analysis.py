57911883512ba3177d22c405d6f322ae627cb64d
nizaz139 <nizarelghazal@gmail.com>
2025-03-23 01:11:14 +0100

    layer sim on allenai c4

--- src/block_similarity/layer_similarity_analysis.py
+++ src/block_similarity/layer_similarity_analysis.py
@@ -33,10 +33,66 @@ def run_layer_similairities(model_path, model_name, dataset_name, batch_size, ma
     
     model.eval()
 
-    dataset = datasets.load_dataset(dataset_name, split="train")
-
+    dataset = datasets.load_dataset(dataset_name, "en", split="train")
     if dataset_size:
         dataset = dataset.select(range(dataset_size))
+    
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, drop_last=True)
+
+    all_distances = [[] for _ in range(model.config.num_hidden_layers - n_layers_to_skip)]
+
+    for batch in tqdm(dataloader, desc="Processing Batches"):
+        inputs = tokenizer(batch["text"], return_tensors="pt", padding="longest", max_length=max_length, truncation=True).to(device)
+
+        with torch.no_grad():
+            outputs = model(**inputs)
+
+        attention_mask = inputs["attention_mask"]
+        hidden_states = outputs.hidden_states
+
+        last_non_padded_hidden_states = get_last_non_padded_tokens(hidden_states, attention_mask)
+
+        # We remove input
+        last_non_padded_hidden_states = last_non_padded_hidden_states[1:]
+
+        block_distances = compute_block_distances(last_non_padded_hidden_states, n_layers_to_skip)
+
+        for i, block_distance in enumerate(block_distances):
+            all_distances[i].append(block_distance)
+
+        del block_distances, batch, last_non_padded_hidden_states
+
+    average_block_distances = [np.mean(distances) for distances in all_distances]
+
+    min_distance = float('inf')  
+    min_distance_layer = 0 
+    
+    os.makedirs("results/", exist_ok=True)
+
+    with open(f'results/layer_distances_{model_name}_{n_layers_to_skip}.csv', 'w', newline='') as csvfile:
+        csvfile.write(f"Analysis of Layer Similarities for {model_name} \n")
+        csvfile.write(f"Number of Layers to Skip : {n_layers_to_skip} \n")
+        fieldnames = ['block_start', 'block_end', 'average_distance']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for i, average_distance in enumerate(average_block_distances):
+            writer.writerow({
+                'block_start': i + 1, 
+                'block_end': i + 1 + n_layers_to_skip,
+                'average_distance': average_distance
+            })
+            
+            if average_distance < min_distance:
+                min_distance = average_distance
+                min_distance_layer = i + 1
+
+        
+        csvfile.write(f"Layer {min_distance_layer} to {min_distance_layer + n_layers_to_skip} is the best block to prune.\n")
+
+
+
+def run_layer_similairities_2(model, tokenizer, dataset, batch_size, max_length, n_layers_to_skip, model_name, device):
+    model.eval()
 
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, drop_last=True)
 
@@ -92,10 +148,6 @@ def run_layer_similairities(model_path, model_name, dataset_name, batch_size, ma
 
 
 
-
-
-
-
 if __name__ == "__main__":
     import argparse 
     # model_path = "facebook/opt-125m"
@@ -108,12 +160,14 @@ if __name__ == "__main__":
 
     # model_path = "mistralai/Mathstral-7B-v0.1"
     model_path = "google/gemma-3-1b-it"
-    dataset_name = "arcee-ai/sec-data-mini"
+    # dataset_name = "arcee-ai/sec-data-mini"
+    dataset_name = "allenai/c4"
+
     model_name = "gemma_1b"
 
-    batch_size = 8
+    batch_size = 32
     max_length = 128
 
     n_layers_to_skip = args.num_layers_skip
 
-    run_layer_similairities(model_path, model_name, dataset_name, batch_size, max_length, n_layers_to_skip)
+    run_layer_similairities(model_path, model_name, dataset_name, batch_size, max_length, n_layers_to_skip, dataset_size=10000)
